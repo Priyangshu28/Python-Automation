ee6e5f7a9706c39e67bcda6daee2d054acd8050f
Priyangshu28 <sarkar.priyangshu2002@gmail.com>
2024-08-30 17:54:38 +0530

    Innitial commit

---
+++ Chapter 1/prg1.py
@@ -0,0 +1,12 @@
+import time
+# This program says hello and asks for my name.
+"""
+print ('Hello, world!')
+print ('hello whats your name')
+my_Name = input()
+print('It is good to meet you, ' + my_Name)
+print('The length of your name is: ' + str(len(my_Name)))
+"""
+my_Age = int(input('What is your age?\n'))
+print('You will be ' + str(my_Age + 1) + ' in a year.')
+time.sleep(1)
